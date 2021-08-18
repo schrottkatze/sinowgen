f7dbd3325adae78911d7bb165a335877300fd911
Gabriel <68819302+obsidianical@users.noreply.github.com>
2021-08-18 23:43:04 +0200

    started splitting code
    trying to refactor for more readability etc

--- src/app.ts
+++ src/app.ts
@@ -4,29 +4,26 @@ import FractalNoise from "./noise/FractalNoise";
 import gBiomeMap from "./biomeMaps/gBiomeMap";
 import robinBiomeMap from "./biomeMaps/robinBiomeMap";
 import { makeNoise2D } from "open-simplex-noise";
+import { textChangeRangeIsUnchanged } from "typescript";
 
-let instance = new p5((p: p5) => {
-  let height = window.innerHeight - 4;
-  let width = window.innerWidth;
+const scale = 0.005;
+const noiseDifference = 0.326578;
+const noiseFactor = 100;
 
-  const seed = 11;
-  const detail = 8;
+const height = 1080;
+const width = 1080;
 
-  p.setup = () => {
+const seed = Math.floor(Math.random() * 100000 + 1);
+const detail = 8;
+
+class App {
+  public static setup(p: p5): void {
     p.createCanvas(width, height);
-    p.background(20, 0, 0);
+    p.background(0, 0, 0);
     p.noStroke();
-  };
+  }
 
-  let debug = 0;
-
-  let iter = 0;
-
-  p.draw = () => {
-    let scale = 0.005;
-    let noiseDifference = 0.326578;
-    let noiseFactor = 100;
-
+  public static draw(p: p5): void {
     const fracNoise = new FractalNoise(seed, detail);
 
     for (let x = 0; x < width; ++x) {
@@ -39,8 +36,17 @@ let instance = new p5((p: p5) => {
     }
 
     p.updatePixels();
-    console.log("done");
-    iter++;
     p.noLoop();
+  }
+}
+
+let instance = new p5((p: p5) => {
+  p.setup = () => {
+    App.setup(p);
+  };
+  p.draw = () => {
+    App.draw(p);
   };
 });
+// let height = window.innerHeight - 4;
+// let width = window.innerWidth;
